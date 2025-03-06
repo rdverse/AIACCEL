68d636ce527eb200d616c91038b3b2a13f1e13da
rdverse <deveshreddy27@gmail.com>
2025-03-05 21:58:08 -0800

    1_2

--- Intro/1_2hello_world.c
+++ Intro/1_2hello_world.c
@@ -1,3 +1,6 @@
+// This program looks at how to create a thread in C using the pthread library
+// Further we will look at synchronization of **data between threads
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stddef.h>
@@ -11,16 +14,17 @@ struct print_message_store{
 
 void *thread_function(void *args ){
 
-    struct print_message_store *p = (struct print_message_store *)args;
+    struct print_message_store *p = (struct print_message_store*)args;
 
     for(int i=0;i<p->number;i++){
         printf("%s %d\n", p->message, i);
     }
+    
     return NULL;
 }
 
-
 int main(){
+    
     pthread_t thread1, thread2;
     struct print_message_store pmsm1 = {"Hello", 3};
     struct print_message_store pmsm2 = {"World", 5};
@@ -38,6 +42,14 @@ int main(){
     // Wait for both threads to finish execution before main thread exits
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+
+    
+    int a =10;
+    int *b = &a;
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", *b);
+    printf("Address of a: %p\n", &a);
+    printf("Address of b: %p\n", &b);
     return 0;
     
 }
