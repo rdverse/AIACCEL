85204663b23495b5408280142eb58ff359fc34e1
rdverse <deveshreddy27@gmail.com>
2025-05-19 11:32:00 -0400

    1d int datagen

--- x_cuda/utils/data_gen.cu
+++ x_cuda/utils/data_gen.cu
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 
+__global__ void simple_1d_gen(int *a, int *b, int n)
+{
+    //a bit of an overkill to do this on the device
+    // but the name of directory is cuda
+    //  :)
+    // For debugging, printing, and other purposes better to do it on host rather
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        a[idx] = static_cast<int>(idx);
+        b[idx] = static_cast<int>(idx+1);}
+    printf("simpledatagen: thread %d, block %d\n", threadIdx.x, blockIdx.x);
+}
+
 __global__ void simple_1d_gen(float *a, float *b, int n)
 {
     //a bit of an overkill to do this on the device
@@ -13,8 +26,7 @@ __global__ void simple_1d_gen(float *a, float *b, int n)
     // For debugging, printing, and other purposes better to do it on host rather
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n) {
-        a[idx] = idx;
-        b[idx] = idx  + 1.0f;
-    }
+        a[idx] = static_cast<float>(idx);
+        b[idx] = static_cast<float>(idx+1);}
     printf("simpledatagen: thread %d, block %d\n", threadIdx.x, blockIdx.x);
 }
